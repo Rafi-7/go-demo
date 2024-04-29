cc7f8e51718d2ed46e3735c3178b38320a9da55c
Rafi-7 <90053674+Rafi-7@users.noreply.github.com>
2024-04-29 22:57:21 +0530

    Add files via upload
    Mongodb implementation

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	opts := options.Client().ApplyURI("mongodb+srv://<username>:<password>@cluster0.up2e714.mongodb.net/")
+	client, _ := mongo.Connect(context.TODO(), opts)
+	defer client.Disconnect(context.Background())
+
+	db := client.Database("SongsCollections")
+	songsCollection := db.Collection("songs")
+
+	song1 := Songs{
+		Name:          "Periyonne Rahmane",
+		Starring:      "Prithivi Raj",
+		Singer:        "A.R.RAHMAN",
+		MusicDirector: "A.R.RAHMAN",
+		Duration:      "05:25",
+		Released:      "2024",
+	}
+
+	song2 := Songs{
+		Name:          "Chaiyya Chaiyya",
+		Starring:      "Shahrukh Khan",
+		Singer:        "Sukhwinder Singh",
+		MusicDirector: "A.R.RAHMAN",
+		Duration:      "07:31",
+		Released:      "1998",
+	}
+
+	song3 := Songs{
+		Name:          "Manjal Veyil",
+		Starring:      "Kamal Hassan, Jyothika",
+		Singer:        "Nakkhul, Hariharan, Vijay",
+		MusicDirector: "Harris Jayaraj",
+		Duration:      "05:55",
+		Released:      "2006",
+	}
+
+	song4 := Songs{
+		Name:          "Yaaradi Nee Mohini",
+		Starring:      "Dhanush, Nayanthara",
+		Singer:        "Udit Narayan",
+		MusicDirector: "Yuvan Shankar Raja",
+		Duration:      "05:03",
+		Released:      "2008",
+	}
+
+	song5 := Songs{
+		Name:          "Life of Ram",
+		Starring:      "Vijay Sethupathi",
+		Singer:        "Pradeep Kumar",
+		MusicDirector: "Govind Vasantha",
+		Duration:      "06:11",
+		Released:      "2018",
+	}
+
+	songList := []interface{}{song1, song2, song3, song4, song5}
+	name, _ := songsCollection.InsertMany(context.Background(), songList)
+	fmt.Println(name)
+
+}
+
+type Songs struct {
+	Name          string
+	Starring      string
+	Singer        string
+	MusicDirector string
+	Duration      string
+	Released      string
+}
